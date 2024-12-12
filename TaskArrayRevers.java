bd7521feffcc5a8bbfdbc6339b543808451d77a5
Harish <127599427+Harish1419@users.noreply.github.com>
2024-12-12 16:32:26 +0530

    Add files via upload

---
+++ TaskArrayRevers.java
@@ -0,0 +1,15 @@
+package Allprogram;
+
+public class TaskArrayRevers {
+	 public static void main(String[] args) {
+	        int[] arr = new int[20];
+	        for (int i = 0; i < 20; i++) {
+	            arr[i] = 20 - i;
+	        }
+
+	        for (int i = 0; i < 20; i++) {
+	            System.out.print(arr[i] + " ");
+	        }
+	    }
+
+}
